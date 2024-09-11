2e844cff13589f4dfecea6bef75c71d45cba439c
kimdahee0815 <billionairedahee@gmail.com>
2024-09-11 10:13:17 +0900

    feat: Big O of stack and queues

---
+++ StacksAndQueues.js
@@ -0,0 +1,13 @@
+// Stack
+
+// google
+// udemy.com
+// youtube
+
+// Stacks
+// Arrays (using push, pop)  good O(1)
+// Linked Lists (using push, pop) good O(1)
+
+// Queues
+// Arrays (using push, shift) bad O(N)
+// Linked Lists (using push, shift) better! O(1)
