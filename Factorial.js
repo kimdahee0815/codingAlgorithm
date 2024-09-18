e9984cbe39eeb9633da5f191470e2113410330b7
kimdahee0815 <billionairedahee@gmail.com>
2024-09-18 12:20:48 +0900

    feat: added big o notation to factorial

--- Factorial.js
+++ Factorial.js
@@ -1,6 +1,7 @@
 // Write two functions that finds the factorial of any number.
 // One should use recursive, the other should just use a for loop
 
+// O(N)
 function findFactorialRecursive(number) {
     if (number === 2) {
         return 2;
@@ -8,6 +9,7 @@ function findFactorialRecursive(number) {
     return number * findFactorialRecursive(number - 1);
 }
 
+// O(N)
 function findFactorialIterative(number) {
     let answer = 1;
     for (let i = 2; i <= number; i++) {
