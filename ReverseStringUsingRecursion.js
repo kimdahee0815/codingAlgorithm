530ad128db479cefc847c11676b06607bd8d7f21
kimdahee0815 <billionairedahee@gmail.com>
2024-09-18 13:22:02 +0900

    feat: reverseString using recursion

---
+++ ReverseStringUsingRecursion.js
@@ -0,0 +1,24 @@
+function reverseString(str) {
+    let result = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        result.push(str[i]);
+    }
+    return result.join("");
+}
+
+console.log(reverseString("yoyo master"));
+
+function reverseStringRecursive(str) {
+    if (str.length === 2) {
+        return str[1] + str[0];
+    }
+    if (str.length === 1) {
+        return str[0];
+    }
+    return (
+        reverseStringRecursive(str.slice(Math.floor(str.length / 2))) +
+        reverseStringRecursive(str.slice(0, Math.floor(str.length / 2)))
+    );
+}
+
+console.log(reverseStringRecursive("this is great"));
