0542a3cb3c28360d499d1fc56e3f1d394f2005d9
kimdahee0815 <billionairedahee@gmail.com>
2024-09-18 12:44:28 +0900

    feat: another iterative way of solving fibonacci

--- Fibonacci.js
+++ Fibonacci.js
@@ -16,6 +16,14 @@ function fibonacciIterative(n) {
     return result;
 }
 
+function fibonacciIterative2(n) {
+    let arr = [0, 1];
+    for (let i = 2; i < n + 1; i++) {
+        arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr[n];
+}
+
 function fibonacciRecursive(n) {
     if (n <= 1) {
         return n;
