bcfe1b431c02acb22b1ef4afd3f2824bbbd09cf5
kimdahee0815 <billionairedahee@gmail.com>
2024-09-20 10:20:55 +0900

    feat: insertionSort2

--- InsertionSort.js
+++ InsertionSort.js
@@ -13,4 +13,19 @@ function insertionSort(array) {
     return array;
 }
 
+function insertionSort2(array) {
+    const length = array.length;
+    for (let i = 0; i < length; i++) {
+        if (array[i] < array[0]) {
+            array.unshift(array.splice(i, 1)[0]);
+        } else {
+            for (let j = 1; j < i; j++) {
+                if (array[i] > array[j - 1] && array[i] < array[j]) {
+                    array.splice(j, 0, array.splice(i, 1)[0]);
+                }
+            }
+        }
+    }
+}
+
 console.log(insertionSort(numbers));
