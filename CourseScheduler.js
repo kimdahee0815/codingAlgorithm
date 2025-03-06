147cf5f3038eb53f5ef05baeb966c0f85038aca8
kimdahee0815 <billionairedahee@gmail.com>
2025-03-06 13:09:00 +0900

    feat: more solutions for course scheduler using topology

--- CourseScheduler.js
+++ CourseScheduler.js
@@ -1,3 +1,4 @@
+//TIME EXCEEDED
 var canFinish = function(numCourses, prerequisites) {
     let queue = [];
     let result = true;
@@ -32,6 +33,7 @@ var canFinish = function(numCourses, prerequisites) {
     return result;
 };
 
+//O(N^3)
 var canFinish2 = function(numCourses, prerequisites) {
   let adjacency = new Array(numCourses).fill(0).map(()=> []);
   for(let i = 0; i < prerequisites.length; i++){
@@ -56,8 +58,32 @@ var canFinish2 = function(numCourses, prerequisites) {
   return result;
 };
 
-console.log(canFinish2(6,[[1,0],[2,1],[2,5],[0,3],[4,3],[3,5],[4,5]]))
-console.log(canFinish2(7,[[0,3],[1,0],[2,1],[4,5],[5,6],[6,4]]))
-console.log(canFinish2(20, [[0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]]))
-console.log(canFinish2(4,[[0,1],[2,3],[1,2],[3,0]]))
-console.log(canFinish2(4,[[2,0],[1,0],[3,1],[3,2],[1,3]]))
+var canFinish3 = function(numCourses, prerequisites) {
+  let adjacency = new Array(numCourses).fill(0).map(()=>[]);
+  let topology = new Array(numCourses).fill(0);
+  for(let i = 0; i< prerequisites.length; i++){
+    adjacency[prerequisites[i][1]].push(prerequisites[i][0]);
+    topology[prerequisites[i][0]] += 1;
+  }
+  let seen = {};
+  for(let i = 0; i< topology.length; ){
+    if(topology[i] === 0 && !seen[i]){
+      let connectedVertexes = adjacency[i];
+      for(let j = 0;j < connectedVertexes.length; j++){
+        topology[connectedVertexes[j]] -= 1;
+      }
+      seen[i] = true;
+      i = 0;
+    }else{
+      i++;
+    }
+  }
+  if(Object.keys(seen).length !== numCourses) return false;
+  return true;
+};
+
+console.log(canFinish3(6,[[1,0],[2,1],[2,5],[0,3],[4,3],[3,5],[4,5]]))
+console.log(canFinish3(7,[[0,3],[1,0],[2,1],[4,5],[5,6],[6,4]]))
+console.log(canFinish3(20, [[0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]]))
+console.log(canFinish3(4,[[0,1],[2,3],[1,2],[3,0]]))
+console.log(canFinish3(4,[[2,0],[1,0],[3,1],[3,2],[1,3]]))
